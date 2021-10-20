966513210e1759f8f1803e792f26d2db0237c8bc
david <davidvalenciaredrovan@gmail.com>
2021-10-19 22:23:22 -0500

    test git launch

--- my_doosan_pkg/launch/my_doosan_gazebo.launch.py
+++ my_doosan_pkg/launch/my_doosan_gazebo.launch.py
@@ -27,13 +27,13 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-	#robot model to option m1013 or a0912 
+	# robot model to option m1013 or a0912
 	
 	robot_model = 'a0912'
 	#robot_model = 'm1013'
 
 	xacro_file = get_package_share_directory('my_doosan_pkg') + '/description'+'/xacro/'+ robot_model +'.urdf.xacro'
-	print (xacro_file)
+	print(xacro_file)
 	
 	# Robot State Publisher 
 	robot_state_publisher = Node(package    ='robot_state_publisher',
@@ -42,7 +42,6 @@ def generate_launch_description():
 								 output     ='both',
 								 parameters =[{'robot_description': Command(['xacro', ' ', xacro_file])           
 								}])
-
 
 	# Spawn the robot in Gazebo
 	spawn_entity_robot = Node(package    ='gazebo_ros', 
@@ -54,7 +53,5 @@ def generate_launch_description():
 	world_file_name = 'my_empty_world.world'
 	world = os.path.join(get_package_share_directory('my_doosan_pkg'), 'worlds', world_file_name)
 	gazebo_node = ExecuteProcess(cmd=['gazebo', '--verbose', world,'-s', 'libgazebo_ros_factory.so'], output='screen')
-	
-
 
 	return LaunchDescription([robot_state_publisher, spawn_entity_robot, gazebo_node ])
